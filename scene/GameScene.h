f5595e177c4c7417b8827fec54b309215e734750
k022g0113@g.neec.ac.jp <k022g0113@g.neec.ac.jp>
2023-04-19 16:42:02 +0900

    02_01_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "Player.h"
 
 /// <summary>
 /// ゲームシーン
@@ -40,10 +41,22 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//自キャラ
+	Player* player_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
