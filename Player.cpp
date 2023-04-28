c5b4bb182b540c3268382d0bc2ea0fd60d3c09a8
k022g0113@g.neec.ac.jp <k022g0113@g.neec.ac.jp>
2023-04-28 11:06:08 +0900

    AL3_02_02

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include <cassert>
+#include <MyMath.h>
+#include "ImGuiManager.h"
 
 void Player::Initialize(Model* model, uint32_t textureHandle) {
 	//NULLポインタチェック
@@ -7,11 +9,55 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	model_ = model;
 	textureHandle_ = textureHandle;
 	worldTransform_.Initialize();
+	//シングルトンインスタンスを取得する
+	input_ = Input::GetInstance();
 }
 
 void Player::Update() {
 	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+
+	//キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+
+	//キャラクターの移動速さ
+	const float kCharacterSpeed = 0.2f;
+
+	//押した方向で移動ベクトルを変更(左右)
+	if (input_->PushKey(DIK_LEFT)) {
+		move.x -= kCharacterSpeed;
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move.x += kCharacterSpeed;
+	}
+
+	////押した方向で移動ベクトルを変更(上下)
+	if (input_->PushKey(DIK_UP)) {
+		move.y += kCharacterSpeed;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		move.y -= kCharacterSpeed;
+	}
+
+	//座標移動(ベクトルの加算)
+	worldTransform_.translation_ = Vec3Addition(worldTransform_.translation_, move);
+
+	// 移動限界座標
+	const float kMoveLimitX = 300;
+	const float kMoveLimitY = 300;
+
+	////範囲を超えない処理
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+	//行列更新
+	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	//キャラクターの座標を画面表示する処理
+	ImGui::Begin("Debug");
+	ImGui::Text("%f. %f. %f", worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
+	ImGui::End();
+
 }
 
 void Player::Draw(ViewProjection& viewProjection_) { 
