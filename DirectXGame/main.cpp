1efc3fac97e22d2dbf97bf0a2d099544dacd729a
k022g0113@g.neec.ac.jp <k022g0113@g.neec.ac.jp>
2023-07-10 14:53:56 +0900

    git

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_08_スヤマ_ハナ_AL3");
+	win->CreateGameWindow();
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
